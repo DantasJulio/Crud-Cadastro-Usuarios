0b8931fe6520142b5dd52a1210721f5b8286cf8e
Julio Dantas <92531119+DantasJulio@users.noreply.github.com>
2022-11-03 11:34:30 -0300

    Update Usuario.java

--- src/main/java/br/com/springboot/curso_jdev_treinamento/model/Usuario.java
+++ src/main/java/br/com/springboot/curso_jdev_treinamento/model/Usuario.java
@@ -8,14 +8,15 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 
-@Entity // sempre definir com a notação @Entity
+@Entity 
 @SequenceGenerator(name = "seq_usuario", sequenceName = "seq_usuario", allocationSize = 1, initialValue = 1) //@SequenceGenerator para criar uma sequencia de id's no banco de dados. Os parametros name, sequenceName, allocationSize, initialValue devem sempre ser passados.
-public class Usuario implements Serializable { // a classe de usuario deve sempre implementar a interface Serializable.
+public class Usuario implements Serializable { 
 
-	private static final long serialVersionUID = 1L; // após implementar a interface, importar o serialVersionUID.
-	
-		@Id // após definir uma variável como o id, usar a notação @Id para definir como primary key.
-		@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_usuario") // usar a notação GenerateValue para criar a sequencia de ids de cada usuario, passando como parametro um sequenceGenerator e o nome da sequenca definido no @SequenceGenerator
+	private static final long serialVersionUID = 1L; 
+		//atributos do usuario:
+		
+		@Id 
+		@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_usuario") 
 		private Long id;
 		private String nome;
 		private int idade;
